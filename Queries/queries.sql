ff69f04c1d64661d28f6cfed7147137f6849eeb3
Brooke Estrada <brookeestrada@Brookes-MacBook-Pro.local>
2021-10-07 23:59:21 -0700

    Added queries for retirment eligibility, updated queries file, and exported csv of retirement candidates.

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -1,2 +1,53 @@
-    -- Query through Departments table.
+-- Query through Departments table.
 SELECT * FROM departments;
+
+-- Remove connetions and data attached to table
+TRUNCATE TABLE dept_employees CASCADE
+
+-- Search all employee names with brithdate in the range.
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+-- Searching for only 1952 Birthdates
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+-- Searching for only 1953 Birthdates
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+-- Searching for only 1954 Birthdates
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+-- Searching for only 1955 Birthdates
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Retirement eligibility converted into table
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- retirement table
+SELECT * FROM retirement_info;
--- schema.sql
+++ schema.sql
@@ -17,7 +17,7 @@ CREATE TABLE employees(
 );
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -26,18 +26,19 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
+CREATE TABLE dept_employees (
+	emp_no INT NOT NULL,
+  	dept_no VARCHAR(4) NOT NULL,
+  	from_date DATE NOT NULL,
+  	to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+  PRIMARY KEY (emp_no, dept_no)
+);
+
 CREATE TABLE salaries (
   emp_no INT NOT NULL,
   salary INT NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
-);
-
-CREATE TABLE dept_employees (
-	emp_no INT NOT NULL,
-  	dept_no INT NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
@@ -50,5 +51,5 @@ CREATE TABLE titles (
 	from_date DATE NOT NULL,
   	to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  PRIMARY KEY (emp_no, title, from_date)
 );
